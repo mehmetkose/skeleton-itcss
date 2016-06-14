db285a78f87db64324b5b00287c682582b9f3798
Mehmet Köse <mehmetkose122@gmail.com>
2016-06-14 07:35:00 +0300

    move source folders

--- gulpfile.js
+++ gulpfile.js
@@ -8,25 +8,23 @@ var sass = require('gulp-sass');
 var settings = require('./package.json');
 
 var scss_files = [
-    'scss/base/base.scss',
-    'scss/components/components.scss',
-    'scss/generic/generic.scss',
-    'scss/objects/objects.scss',
-    'scss/settings/settings.scss',
-    'scss/tools/tools.scss',
-    'scss/trumps/trumps.scss'
+    'app/scss/base/base.scss',
+    'app/scss/components/components.scss',
+    'app/scss/generic/generic.scss',
+    'app/scss/objects/objects.scss',
+    'app/scss/settings/settings.scss',
+    'app/scss/tools/tools.scss',
+    'app/scss/trumps/trumps.scss'
 ]
-gulp.task('scss-task', function () {
+gulp.task('scss', function () {
     return gulp.src(scss_files)
-        .pipe(sass({
-            outputStyle: 'compressed',
-            errLogToConsole: true}).on('error', sass.logError))
+        .pipe(sass({outputStyle: 'compressed', errLogToConsole: true}).on('error', sass.logError))
         .pipe(concat('style.min.css'))
-        .pipe(gulp.dest('css/'))
+        .pipe(gulp.dest('public/css/'))
 });
 
 gulp.task('watch', function () {
-    gulp.watch(["scss/*/*.scss"], ['scss']);
+    gulp.watch(["app/scss/*/*.scss"], ['scss']);
 });
 
-gulp.task('default', ['scss-task','watch'])
+gulp.task('default', ['scss','watch'])
